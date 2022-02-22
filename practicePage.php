15b56b691e0a5d320824c31adc5c1b1a80743995
Gee Hunter <gh@localhost.localdomain>
2022-02-22 16:17:38 -0500

    finalized flexbox sample

---
+++ practicePage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE Html>
+<html>
+    <head>
+        <title>Flexbox</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="flexboxStyles.css"
+    </head>
+    <body>
+        <main>
+            <header>
+                <nav>
+                    <ul>
+                        <li><link a href="aboutUs.php">About Us</a>  </li>
+                    </ul>
+                </nav>
+
+            </header>
+            <article class="cardContainer">
+                <div class="card1">
+                    <div id="box1"></div>
+                        <div id="btn">
+                        <button type="submit" name="submit" value="submit">OK</button></div>
+                        </div>
+
+                </div>
+                <div class="card2">
+                    <div id="box1"></div>
+                    
+
+                </div>
+            </article>
+            <footer>
+
+            </footer>
+        </main>
+    </body>
+</html>
